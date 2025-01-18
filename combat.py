e94d990b42d5c020010c14e7510f1aa65b17ad71
Rob Ravosa <roravos@gmail.com>
2025-01-17 22:36:03 -0500

    Made some comments to clear up implementation

--- combat.py
+++ combat.py
@@ -4,6 +4,7 @@ from playerturn import *
 from enemyturn import *
 from item import *
 
+#Temporary. Will be created in adventure.py for final implementation
 p = player(10)
 e = enemy(10)
 pturn = playerturn(p,e)
@@ -11,6 +12,7 @@ eturn = enemyturn(p,e)
 i = item(3, "Healing",p)
 p.addItem(i)
 
+#incomplete. will be in a class and have its own method
 while (p.hitPoints > 0 and e.hitPoints > 0):
     print(p.hitPoints,",",e.hitPoints) #Only implemented for testing
     pturn.action()
